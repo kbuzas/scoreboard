223756c0331cb525c4720a371ba24f1507ed8aa9
kbuzas <mRVgggDN008!>
2024-10-28 14:43:57 +0100

    Fixed TeamPairTest

--- src/main/java/model/TeamPair.java
+++ src/main/java/model/TeamPair.java
@@ -19,5 +19,22 @@ public class TeamPair {
         return awayTeam;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TeamPair teamPair = (TeamPair) o;
+
+        if (!getHomeTeam().equals(teamPair.getHomeTeam())) return false;
+        return getAwayTeam().equals(teamPair.getAwayTeam());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getHomeTeam().hashCode();
+        result = 31 * result + getAwayTeam().hashCode();
+        return result;
+    }
 }
 
